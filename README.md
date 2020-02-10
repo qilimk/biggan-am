eedd99470997fea18dde3e8f8df762abc88959f8
Michael A. Alcorn <airalcorn2@gmail.com>
2020-02-10 17:49:45 -0600

    Initial re-factoring.

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # BigGAN-AM
 
-This code release accompanies the following paper: 
+This code release accompanies the following paper:
 
 ## Improving sample diversity of a pre-trained, class-conditional GAN by changing its class embeddings \[[Video](https://youtu.be/y5bDc-dbNjg)\]  \[[arXiv](https://arxiv.org/abs/1910.04760)\]
 
 Qi Li, Long Mai, Anh Nguyen
 
-### Abstract: 
+### Abstract
 Mode collapse is a well-known issue with Generative Adversarial Networks (GANs) and is a byproduct of unstable GAN training. We propose to improve the sample diversity of a pre-trained class-conditional generator by modifying its class embeddings in the direction of maximizing the log probability outputs of a classifier pre-trained on the same dataset. We improved the sample diversity of state-of-the-art ImageNet BigGANs at both 128 x 128 and 256 x 256 resolutions. By replacing the embeddings, we can also synthesize plausible images for Places365 using a BigGAN pre-trained on ImageNet.
 
 ![ceaser](/doc/ceaser_daisy.png)
@@ -15,27 +15,25 @@ Mode collapse is a well-known issue with Generative Adversarial Networks (GANs) 
 
 ![synthesize_Places365](/doc/synthesize_new_dataset_images.png)
 
-## Requirments:
+## Requirments
 
-### Python
-* python 3.6
-* Pytorch
-* numpy
-* torchvision
-* pillow
-
-### BigGAN
-
-### robustness package
-Please install robustness package from [MadryLab](https://github.com/MadryLab/robustness)
+* Python 3.6
+* PyTorch
+* NumPy
+* `torchvision`
+* Pillow
+* [`robustness`](https://github.com/MadryLab/robustness)
 
 ## Getting Started
 
-### Pretraind Models
-Please download the pretrained BigGAN model from [BigGAN-PyTorch](https://drive.google.com/file/d/1nAle7FCVFZdix2--ks0r5JBkFnKw8ctW/view)
+### Pre-trained Models
+Please download the pre-trained BigGAN model from [BigGAN-PyTorch](https://drive.google.com/file/d/1nAle7FCVFZdix2--ks0r5JBkFnKw8ctW/view).
 
 ### Run the optimization
-`python opt_c_embedding_over_multi_z.py`
+
+```bash
+python3 opt_c_embedding_over_multi_z.py
+```
 
 ## Citation
 If you find this work useful for your research, please consider citing:
---
+++ biggan_am_utils.py
@@ -0,0 +1,271 @@
+import argparse
+import datetime
+import json
+import os
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+from robustness import datasets, model_utils
+from user_constants import DATA_PATH_DICT
+
+
+def get_config(resolution):
+    attn_dict = {128: "64", 256: "128", 512: "64"}
+    dim_z_dict = {128: 120, 256: 140, 512: 128}
+    return {
+        "G_param": "SN",
+        "D_param": "SN",
+        "G_ch": 96,
+        "D_ch": 96,
+        "D_wide": True,
+        "G_shared": True,
+        "shared_dim": 128,
+        "dim_z": dim_z_dict[resolution],
+        "hier": True,
+        "cross_replica": False,
+        "mybn": False,
+        "G_activation": nn.ReLU(inplace=True),
+        "G_attn": attn_dict[resolution],
+        "norm_style": "bn",
+        "G_init": "ortho",
+        "skip_init": True,
+        "no_optim": True,
+        "G_fp16": False,
+        "G_mixed_precision": False,
+        "accumulate_stats": False,
+        "num_standing_accumulations": 16,
+        "G_eval_mode": True,
+        "BN_eps": 1e-04,
+        "SN_eps": 1e-04,
+        "num_G_SVs": 1,
+        "num_G_SV_itrs": 1,
+        "resolution": resolution,
+        "n_classes": 1000,
+    }
+
+
+def load_mit(model_name):
+    model_file = f"{model_name}_places365.pth.tar"
+    if not os.access(model_file, os.W_OK):
+        weight_url = f"http://places2.csail.mit.edu/models_places365/{model_file}"
+        os.system(f"wget {weight_url}")
+
+    model = models.__dict__[model_name](num_classes=365)
+    checkpoint = torch.load(model_file, map_location=lambda storage, loc: storage)
+    state_dict = {
+        str.replace(k, "module.", ""): v for k, v in checkpoint["state_dict"].items()
+    }
+    model.load_state_dict(state_dict)
+
+    return model
+
+
+def load_madrylab_imagenet(arch):
+    data = "ImageNet"
+    arch = arch
+    dataset_function = getattr(datasets, data)
+    dataset = dataset_function(DATA_PATH_DICT[data])
+    model_kwargs = {
+        "arch": arch,
+        "dataset": dataset,
+        "resume_path": f"./madrylab_models/{data}.pt",
+        "state_dict_path": "model",
+    }
+    (model, _) = model_utils.make_and_restore_model(**model_kwargs)
+
+    return model
+
+
+def load_net(model_name):
+    print(f"Loading {model_name} classifier...")
+    if model_name == "resnet50":
+        return models.resnet50(pretrained=True)
+
+    elif model_name == "alexnet":
+        return models.alexnet(pretrained=True)
+
+    elif model_name == "alexnet_conv5":
+        return models.alexnet(pretrained=True).features
+
+    elif model_name == "inception_v3":
+        # Modified the original file in torchvision/models/inception.py !!!
+        return models.inception_v3(pretrained=True)
+
+    elif model_name == "mit_alexnet":
+        return load_mit("alexnet")
+
+    elif model_name == "mit_resnet18":
+        return load_mit("resnet18")
+
+    elif model_name == "madrylab_resnet50":
+        return load_madrylab_imagenet("resnet50")
+
+    else:
+        raise ValueError(f"{model_name} is not a supported classifier...")
+
+
+def save_intermediate_data(plot_data, path):
+    json_save = json.dumps(plot_data)
+    f = open(path, "w")
+    f.write(json_save)
+    f.close()
+
+
+def parse_options():
+    parser = argparse.ArgumentParser(description="Optimizing the process with seed")
+
+    parser.add_argument(
+        "--seed_z", type=int, default=0, help="Random seed for z to use"
+    )
+
+    parser.add_argument(
+        "--ini_y_num", type=int, default=5, help="the number of initial y"
+    )
+
+    parser.add_argument(
+        "--ini_y_method",
+        type=str,
+        default="random",
+        help="the methods to intialize the y: random/one_hot/mean_random",
+    )
+
+    parser.add_argument("--lr", type=float, default=0.1, help="Initial learning rate")
+
+    parser.add_argument(
+        "--dr",
+        type=float,
+        default=0.9,
+        help="Weight decay rate by using adam optimizer",
+    )
+
+    parser.add_argument(
+        "--n_iters", type=int, default=100, help="the number of iterations"
+    )
+
+    parser.add_argument("--z_num", type=int, default=10, help="the number of z")
+
+    parser.add_argument(
+        "--steps_per_z", type=int, default=50, help="the number of iterations"
+    )
+
+    parser.add_argument("--cuda", type=int, default=0, help="the index of gpu3")
+
+    parser.add_argument(
+        "--model", type=str, default="alexnet", help="the classifier model"
+    )
+
+    parser.add_argument(
+        "--resolution", type=int, default=256, help="the resolution of BigGAN output"
+    )
+
+    parser.add_argument(
+        "--gaussian_var",
+        type=float,
+        default=1.0,
+        help="The variance of gaussian distribution for random initialization ",
+    )
+
+    parser.add_argument(
+        "--experiment_name",
+        type=str,
+        default="E1",
+        help="program will produce the output folder based on experiment name.",
+    )
+
+    parser.add_argument(
+        "--ini_onehot_method",
+        type=str,
+        default="top",
+        help="the methods to generate one hot classes: top/random",
+    )
+
+    parser.add_argument(
+        "--with_dloss",
+        type=bool,
+        default=False,
+        help="add diversity loss to total loss function if True",
+    )
+
+    parser.add_argument(
+        "--dloss_funtion",
+        type=str,
+        default="softmax",
+        help="use different diversity loss functions which are softmax/pixelwise/features",
+    )
+
+    parser.add_argument(
+        "--alpha",
+        type=float,
+        default=0.1,
+        help="The coefficient of diversity loss term",
+    )
+
+    parser.add_argument(
+        "--noise_std",
+        type=float,
+        default=0,
+        help="The std of gaussian add to one hot origin initialization.",
+    )
+
+    parser.add_argument(
+        "--weight_path",
+        type=str,
+        default="pretrained_weights/E7_G_ema_138000_original.pth",
+        help="The std of gaussian add to one hot origin initialization.",
+    )
+
+    parser.add_argument(
+        "--class_list",
+        type=str,
+        default="../head_50_random_class.txt",
+        help="class list to optimize",
+    )
+
+    return parser.parse_args()
+
+
+def save_files(
+    experiment_name,
+    model,
+    target_class,
+    index_class,
+    ini_y_method,
+    ini_onehot_method,
+    one_hot,
+    n_iters,
+    z_num,
+    steps_per_z,
+    lr,
+    alpha,
+    dloss_function,
+    seed_z,
+):
+    # Create the output folder
+    dt = datetime.datetime.now()
+    dir_name = f"./{experiment_name}_{model}_opt_y_over_z/opt_y_multi_z_target_"
+    dir_name += f"{target_class:0=3d}_optmethod_{ini_y_method}_"
+    if one_hot:
+        dir_name += f"{ini_onehot_method}_"
+
+    dir_name += f"iter_{n_iters:0=3d}_znum_{z_num:0=3d}_optnum_{steps_per_z:0=3d}_"
+    dir_name += f"lr_{lr:.6f}_alpha_{alpha:.3f}_dloss_funtion_{dloss_function}_"
+    dir_name += f"{dt:%m%d%H%M%S}"
+
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+
+    file_extent = f"target_{target_class:0=3d}_"
+    if one_hot:
+        file_extent += f"onehot_{index_class:0=3d}_"
+
+    file_extent += f"iter_{n_iters:0=3d}_znum_{z_num:0=3d}_optnum_{steps_per_z:0=3d}_"
+    file_extent += f"lr_{lr:.6f}_seed_{seed_z:0=2d}_{dt:%m%d%H%M%S}"
+
+    filename_y_save = f"{dir_name}/save_{model}_opt_y_{file_extent}"
+    filename_z_save = f"{dir_name}/save_{model}_opt_z_{file_extent}"
+    intermediate_data_save = (
+        f"{dir_name}/intermediate_data_{model}_opt_y_{file_extent}.json"
+    )
+
+    return (dir_name, filename_y_save, filename_z_save, intermediate_data_save)
